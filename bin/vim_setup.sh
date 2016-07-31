a05cb1ff1a1eca967a2cca63c42c9264c87c4cac
Alexandre Tea <alexandre.qtea@gmail.com>
2016-07-31 11:24:34 +0400

    add plugin indent_guides

--- bin/vim_setup.sh
+++ bin/vim_setup.sh
@@ -85,3 +85,13 @@ set background=dark
 let g:solarized_termcolors=256
 colorscheme solarized"
 
+# indent guides
+pathogen_install "https://github.com/nathanaelkane/vim-indent-guides" "
+\" Indent Guides settings
+    \" indent_guides {
+        if isdirectory(expand(\"~/.vim/bundle/vim-indent-guides/\"))
+            let g:indent_guides_start_level = 2
+            let g:indent_guides_guide_size = 1
+            let g:indent_guides_enable_on_vim_startup = 1
+        endif
+    \" }"
