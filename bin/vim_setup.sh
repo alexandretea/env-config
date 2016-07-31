8e20ea4c91dcb4419af517e7dbc41f344520bcc3
Alexandre Tea <alexandre.qtea@gmail.com>
2016-08-01 01:21:49 +0400

    put bundles settings into file vimrc.bundles + add plugin autoclose and better whitespace

--- bin/vim_setup.sh
+++ bin/vim_setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # script to setup my vim configuration
+# TODO snippet engine
+# TODO header
+# TODO persistent undo
+# TODO describe blabla inspired from spf13 blabla
 
 # function to install a plugin with pathogen
 # first param is the git url, second is the settings
 pathogen_install() {
     url=$1
-    settings=$2
     target_name=$(basename "$url")
     target_path=~/.vim/bundle/$target_name
 
@@ -15,44 +18,37 @@ pathogen_install() {
     else
         echo "| Installing plugin '$target_name'"
         git clone "$url" "$target_path"
-        if [ ${#settings} -gt 0 ]; then
-            echo "$settings" >> ~/.vimrc
-        fi
     fi
 }
 
-mkdir -p ~/.vim && mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim && mkdir -p ~/.vim/bundle && mkdir -p ~/.vim/snippets
 
 
 # install pathogen (useful to easily install vim plugins)
 target=~/.vim/autoload/pathogen.vim
 if [ ! -e $target ]; then
     mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-        curl -LSso $target https://tpo.pe/pathogen.vim && \
-        echo "
-    \" Pathogen load
-    filetype off
-
-    call pathogen#infect()
-    call pathogen#helptags()
-
-    filetype plugin indent on
-    syntax on" >> ~/.vimrc
+        curl -LSso $target https://tpo.pe/pathogen.vim
 else
     echo "| Pathogen is already installed"
 fi
 
 
-# install plugins without settings
+# Plugins installs
 declare -a plugins=(
-"https://github.com/scrooloose/nerdtree"            # filesystem browsing
-"https://github.com/scrooloose/nerdcommenter"       # easy comenting
-"https://github.com/kien/ctrlp.vim"                 # ctrlp (fuzzy file finder)
-"https://github.com/scrooloose/syntastic"           # syntastic (syntax checker)
-"https://github.com/bling/vim-airline"              # vim-airline (custom status/tabline)
-"https://github.com/klen/python-mode"               # python mode
-"https://github.com/terryma/vim-multiple-cursors"   # vim-multiple-cursor
-"https://github.com/tpope/vim-abolish"              # search and substitute
+    "https://github.com/altercation/vim-colors-solarized"   # solarized colorscheme
+    "https://github.com/scrooloose/nerdtree"                # filesystem browsing
+    "https://github.com/scrooloose/nerdcommenter"           # easy comenting
+    "https://github.com/kien/ctrlp.vim"                     # ctrlp (fuzzy file finder)
+    "https://github.com/scrooloose/syntastic"               # syntastic (syntax checker)
+    "https://github.com/bling/vim-airline"                  # vim-airline (custom status/tabline)
+    "https://github.com/klen/python-mode"                   # python mode
+    "https://github.com/terryma/vim-multiple-cursors"       # vim-multiple-cursor
+    "https://github.com/tpope/vim-abolish"                  # search and substitute
+    "https://github.com/vitalk/vim-shebang"                 # shebangs
+    "https://github.com/ntpeters/vim-better-whitespace"     # highlights/strips trailing whitespaces
+    "https://github.com/nathanaelkane/vim-indent-guides"    # show indent levels
+    "https://github.com/townk/vim-autoclose"                # automatically closes ([{
 )
 
 for plugin in "${plugins[@]}"
@@ -61,7 +57,7 @@ do
 done
 
 
-# install plugins with settings or specific installation
+# Manual installs
 
 # youcompleteme (code-completion engine)
 # /!\ need the following packages:
@@ -76,26 +72,3 @@ git clone https://github.com/valloric/youcompleteme $target \
 else
     echo "| Plugin 'YouCompleteMe' is already installed"
 fi
-
-# solarized color scheme
-pathogen_install "https://github.com/altercation/vim-colors-solarized" "
-\" Solarized settings
-syntax enable
-set background=dark
-let g:solarized_termcolors=256
-colorscheme solarized"
-
-# indent guides
-pathogen_install "https://github.com/nathanaelkane/vim-indent-guides" "
-\" Indent Guides settings
-    \" indent_guides {
-        if isdirectory(expand(\"~/.vim/bundle/vim-indent-guides/\"))
-            let g:indent_guides_start_level = 2
-            let g:indent_guides_guide_size = 1
-            let g:indent_guides_enable_on_vim_startup = 1
-
-            let g:indent_guides_auto_colors = 0
-            autocmd VimEnter,Colorscheme * :hi IndentGuidesOdd  ctermbg=black
-            autocmd VimEnter,Colorscheme * :hi IndentGuidesEven ctermbg=darkgrey 
-        endif
-    \" }"
