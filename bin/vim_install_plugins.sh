56ce7463b2b0f037a5765cacf4e713211c749e80
Alexandre Tea <alexandre.qtea@gmail.com>
2016-08-02 01:23:36 +0400

    rename old vim_setup to vim_isntall_plugins, create vim_setup to setup vim config files

---
+++ bin/vim_install_plugins.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# script to setup my vim configuration
+# TODO snippet engine
+# TODO header
+# TODO describe blabla inspired from spf13 blabla
+
+# function to install a plugin with pathogen
+# first param is the git url, second is the settings
+pathogen_install() {
+    url=$1
+    target_name=$(basename "$url")
+    target_path=~/.vim/bundle/$target_name
+
+    if [ -e $target_path ]; then
+        echo "| Plugin '$target_name' is already installed"
+    else
+        echo "| Installing plugin '$target_name'"
+        git clone "$url" "$target_path"
+    fi
+}
+
+mkdir -p ~/.vim && mkdir -p ~/.vim/bundle && mkdir -p ~/.vim/snippets
+
+
+# install pathogen (useful to easily install vim plugins)
+target=~/.vim/autoload/pathogen.vim
+if [ ! -e $target ]; then
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+        curl -LSso $target https://tpo.pe/pathogen.vim
+else
+    echo "| Pathogen is already installed"
+fi
+
+
+# Plugins installs
+declare -a plugins=(
+    "https://github.com/altercation/vim-colors-solarized"   # solarized colorscheme
+    "https://github.com/scrooloose/nerdtree"                # filesystem browsing
+    "https://github.com/scrooloose/nerdcommenter"           # easy comenting
+    "https://github.com/kien/ctrlp.vim"                     # ctrlp (fuzzy file finder)
+    "https://github.com/scrooloose/syntastic"               # syntastic (syntax checker)
+    "https://github.com/bling/vim-airline"                  # vim-airline (custom status/tabline)
+    "https://github.com/klen/python-mode"                   # python mode
+    "https://github.com/terryma/vim-multiple-cursors"       # vim-multiple-cursor
+    "https://github.com/tpope/vim-abolish"                  # search and substitute
+    "https://github.com/vitalk/vim-shebang"                 # shebangs
+    "https://github.com/ntpeters/vim-better-whitespace"     # highlights/strips trailing whitespaces
+    "https://github.com/nathanaelkane/vim-indent-guides"    # show indent levels
+    "https://github.com/townk/vim-autoclose"                # automatically closes ([{
+    "https://github.com/mbbill/undotree"                    # undo tree, persistent undo
+)
+
+for plugin in "${plugins[@]}"
+do
+    pathogen_install "$plugin"
+done
+
+
+# Manual installs
+
+# youcompleteme (code-completion engine)
+# /!\ need the following packages:
+# automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel
+target=~/.vim/bundle/youcompleteme
+if [ ! -e "$target" ]; then
+git clone https://github.com/valloric/youcompleteme $target \
+    && cd ~/.vim/bundle/youcompleteme \
+    && git submodule update --init --recursive \
+    && ./install.py --clang-completer \
+    && cd -
+else
+    echo "| Plugin 'YouCompleteMe' is already installed"
+fi
--- bin/vim_setup.sh
+++ bin/vim_setup.sh
@@ -1,74 +1,30 @@
 #!/bin/bash
 
-# script to setup my vim configuration
-# TODO snippet engine
-# TODO header
-# TODO describe blabla inspired from spf13 blabla
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# function to install a plugin with pathogen
-# first param is the git url, second is the settings
-pathogen_install() {
-    url=$1
-    target_name=$(basename "$url")
-    target_path=~/.vim/bundle/$target_name
-
-    if [ -e $target_path ]; then
-        echo "| Plugin '$target_name' is already installed"
-    else
-        echo "| Installing plugin '$target_name'"
-        git clone "$url" "$target_path"
-    fi
-}
-
-mkdir -p ~/.vim && mkdir -p ~/.vim/bundle && mkdir -p ~/.vim/snippets
-
-
-# install pathogen (useful to easily install vim plugins)
-target=~/.vim/autoload/pathogen.vim
-if [ ! -e $target ]; then
-    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-        curl -LSso $target https://tpo.pe/pathogen.vim
-else
-    echo "| Pathogen is already installed"
-fi
-
-
-# Plugins installs
-declare -a plugins=(
-    "https://github.com/altercation/vim-colors-solarized"   # solarized colorscheme
-    "https://github.com/scrooloose/nerdtree"                # filesystem browsing
-    "https://github.com/scrooloose/nerdcommenter"           # easy comenting
-    "https://github.com/kien/ctrlp.vim"                     # ctrlp (fuzzy file finder)
-    "https://github.com/scrooloose/syntastic"               # syntastic (syntax checker)
-    "https://github.com/bling/vim-airline"                  # vim-airline (custom status/tabline)
-    "https://github.com/klen/python-mode"                   # python mode
-    "https://github.com/terryma/vim-multiple-cursors"       # vim-multiple-cursor
-    "https://github.com/tpope/vim-abolish"                  # search and substitute
-    "https://github.com/vitalk/vim-shebang"                 # shebangs
-    "https://github.com/ntpeters/vim-better-whitespace"     # highlights/strips trailing whitespaces
-    "https://github.com/nathanaelkane/vim-indent-guides"    # show indent levels
-    "https://github.com/townk/vim-autoclose"                # automatically closes ([{
-    "https://github.com/mbbill/undotree"                    # undo tree, persistent undo
+declare -a files_paths=(
+    "$BASEDIR/../config/vimrc"
+    "$BASEDIR/../config/vimrc.bundles"
 )
+declare -a link_paths=(
+    "~/.vimrc"
+    "~/.vimrc.bundles"
+)
+clean=true
 
-for plugin in "${plugins[@]}"
+for i in `seq 0 $((${#link_paths[@]} - 1))`
 do
-    pathogen_install "$plugin"
+    file=${files_paths[$i]}
+    link_path=${link_paths[$i]}
+    link_path="${link_path/#\~/$HOME}"
+    if [ -f $link_path ] && [ "$1" != "--force" ]; then
+        echo "Error: File '$link_path' already exists"
+        clean=false
+    else
+        ln -sf $file $link_path
+    fi
 done
 
-
-# Manual installs
-
-# youcompleteme (code-completion engine)
-# /!\ need the following packages:
-# automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel
-target=~/.vim/bundle/youcompleteme
-if [ ! -e "$target" ]; then
-git clone https://github.com/valloric/youcompleteme $target \
-    && cd ~/.vim/bundle/youcompleteme \
-    && git submodule update --init --recursive \
-    && ./install.py --clang-completer \
-    && cd -
-else
-    echo "| Plugin 'YouCompleteMe' is already installed"
+if ! $clean; then
+    echo "Use --force to ignore and override existant files"
 fi
